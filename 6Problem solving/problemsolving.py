81631cfe573989590238c3dcc4f29e937d96561d
Ry8055 <89428151+Ry8055@users.noreply.github.com>
2024-08-25 03:31:56 +0000

    newfile

---
+++ 6Problem solving/problemsolving.py
@@ -0,0 +1,53 @@
+# # Ques 1 . write a program to display person name , age , and address in diffrent line .
+# name = "john"
+# age = 23
+# address = "Indore Madhya Pradesh"
+# print(name ,"\n",age , "\n",address)
+
+# Ques 2 . write a program to swap two variable .
+# 1st method 
+# this we can do with 3rd temporary variable . 
+# x = 12 
+# y = 15 
+# temp = x
+# x = y
+# y = temp
+# print("value of x",x)
+# print("value of y",y)
+
+# # 2nd method 
+# # left,right = right,left
+# a = 23
+# b = 45
+# a ,b = b, a
+# print ("value of a",a)
+# print ("value of b",b)
+
+
+
+# # Ques 3 . write a program to convert float into integer .
+# x = 23.3
+# print(x,type(x))
+# # we need to do typecasting
+# x = int(x)
+# print(x,type(x))
+
+
+
+# # Ques 4 . write a program to take details from student for id card and then print it in diffrent line . 
+# name = input("enter name of student : ")
+# grade = input("enter grade of student: ")
+# age = int(input("enter age of student: "))
+# email = input("enter email of student: ")
+# ph_no = int(input("enter phone number of student: "))
+# print("student identity card")
+# print(name,"\n",grade,"\n",age,"\n",email,"\n",ph_no)
+
+
+# Ques 5 . write a program to take user input as integer then convert it to float . 
+num = int(input(print("enter a number ")))
+print(type(num))
+print("convert it to float")
+num = float(num)
+print(num,type(num))
+
